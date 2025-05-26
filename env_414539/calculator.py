838785d0f04e232d8e109f74ecac4741ac2541ab
WJurgiel <wjurgiel@student.agh.edu.pl>
2025-05-26 12:10:08 +0200

    worflow test 2

--- env_414539/calculator.py
+++ env_414539/calculator.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 def dodawanie(liczba1, liczba2):
     return np.add(liczba1, liczba2)
@@ -14,3 +15,23 @@ def dzielenie(liczba1, liczba2):
         return np.divide(liczba1, liczba2)
     else:
         return "Błąd: dzielenie przez zero!"
+
+def power_of_two(number):
+    return number ** 2
+
+def create_dataset(dataset):
+    data = pd.DataFrame(dataset)
+    return data
+
+def get_mean_price(data):
+    if 'price' in data.columns: 
+        return data.price.mean()
+    return -1
+
+def get_blue_cars(data):
+    if 'color' and 'cars' in data.columns:
+        return data[data['color'] == 'blue'][['cars', 'price']]
+    return -1
+
+def get_doors_count(data):
+    return sum(data['doors'])
--- env_414539/calculator_test.py
+++ env_414539/calculator_test.py
@@ -1,4 +1,5 @@
-from calculator import dodawanie,dzielenie,mnozenie,odejmowanie
+from calculator import dodawanie,dzielenie,mnozenie,odejmowanie, create_dataset, get_mean_price, get_blue_cars,get_doors_count
+import pandas as pd
 def test_dodawanie():
     assert dodawanie(2, 3) == 5
     assert dodawanie(-1, 1) == 0
@@ -15,3 +16,34 @@ def test_dzielenie():
     assert dzielenie(6, 3) == 2
     assert dzielenie(1, -1) == -1
     assert dzielenie(1, 0) == "Błąd: dzielenie przez zero!"
+
+dataset = {
+    'cars': ['Audi', 'BMW', 'Peugeot', 'Ford'],
+    'color': ['red', 'black', 'blue', 'blue'],
+    'doors': [5,3,5,5],
+    'price': [6000,4500, 3000, 6500]
+}
+
+def test_create_dataset():
+    data = create_dataset(dataset)
+    assert data.shape == (4,4)
+    assert data.size == 16
+
+def test_get_mean_price():
+    data = create_dataset(dataset)
+    mean = get_mean_price(data)
+    assert mean != -1
+    assert mean == 5000
+
+def test_get_blue_cars():
+    data = create_dataset(dataset)
+    expected = pd.DataFrame({
+        'cars': ['Peugeot', 'Ford'],
+        'price': [3000, 6500]
+    })
+    result = get_blue_cars(data)
+    assert result.reset_index(drop=True).equals(expected.reset_index(drop=True))
+
+def test_get_doors_count():
+    data = create_dataset(dataset)
+    assert get_doors_count(data) == 18
